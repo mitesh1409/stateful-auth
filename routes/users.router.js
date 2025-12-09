2064c9c58f18fb719b2e9e3adcbe4a9be1281b71
Mitesh Prajapati <mitesh5.mprajapati@gmail.com>
2025-12-09 16:58:47 +0530

    🚀

--- routes/users.router.js
+++ routes/users.router.js
@@ -2,14 +2,15 @@ import express from 'express';
 
 import * as UsersController from '../controllers/users.controller.js';
 import authenticate from '../middlewares/authenticate.js';
+import ifLoggedIn from '../middlewares/ifLoggedIn.js';
 
 const usersRouter = express.Router();
 
-usersRouter.get('/sign-up', UsersController.signUp);
-usersRouter.post('/sign-up', UsersController.doSignUp);
+usersRouter.get('/sign-up', ifLoggedIn, UsersController.signUp);
+usersRouter.post('/sign-up', ifLoggedIn, UsersController.doSignUp);
 
-usersRouter.get('/sign-in', UsersController.signIn);
-usersRouter.post('/sign-in', UsersController.doSignIn);
+usersRouter.get('/sign-in', ifLoggedIn, UsersController.signIn);
+usersRouter.post('/sign-in', ifLoggedIn, UsersController.doSignIn);
 
 usersRouter.get('/dashboard', authenticate, UsersController.dashboard);
 
